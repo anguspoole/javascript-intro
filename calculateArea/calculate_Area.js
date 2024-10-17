dbd6ac6b01c8d8b0a812b02e3537471cd13aa3d6
anguspoole <anguspoole@gmail.com>
2024-10-17 13:02:06 -0400

    Adding grocery calculation by passing in values as parameters directly from the html file

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,12 @@ function calculateArea() {
  let area = length * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let price1, price2, price3;
+function calculateAmount(amount1, amount2, amount3) {
+ price1 = parseFloat(amount1);
+ price2 = parseFloat(amount2);
+ price3 = parseFloat(amount3);
+
+ document.getElementById('total').innerText = `The price of these groceries is: $${price1 + price2 + price3}`;
+}
